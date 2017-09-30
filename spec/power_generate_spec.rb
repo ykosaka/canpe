f54bc07c6dc6407e6988370234e9d5408c2fbb6b
Yoshinori Kosaka <yoshinori.ksk@gmail.com>
2017-09-30 10:09:29 +0900

    comment out gemspec file

--- spec/power_generate_spec.rb
+++ spec/power_generate_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe PowerGenerate do
   it "has a version number" do
     expect(PowerGenerate::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
