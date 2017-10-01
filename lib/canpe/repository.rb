02523e0917678a1cdfa43bcd70773733897e8722
Yoshinori Kosaka <yoshinori.ksk@gmail.com>
2017-10-02 00:34:48 +0900

    load yaml file

--- lib/canpe/repository.rb
+++ lib/canpe/repository.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'yaml'
 require 'fileutils'
 require 'pry'
 require 'canpe/repository_store'
@@ -12,12 +13,22 @@ module Canpe
       @name = name
     end
 
+    # Directory which is root directory of this repository
     def repository_url
       File.join(RepositoryStore.repositories_root, name)
     end
 
+    # Directory in which templates file exists
     def templates_url
       File.join(RepositoryStore.repositories_root, name, 'templates')
+    end
+
+    def binding_option_url
+      File.join(repository_url, 'binding.yml')
+    end
+
+    def binding_options
+      @_binding_options = YAML.load_file(binding_option_url)
     end
 
     def file_paths(absolute_path: false)
