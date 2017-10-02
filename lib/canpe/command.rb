d2c1bbe8fc6e036b18ad790eb3144593207a6425
Yoshinori Kosaka <yoshinori.ksk@gmail.com>
2017-10-03 03:28:56 +0900

    add open command

--- lib/canpe/command.rb
+++ lib/canpe/command.rb
@@ -7,6 +7,7 @@ require 'canpe/repository_store'
 class Canpe::Command < Thor
 
   desc "generate repo_name", "Generate files from repository"
+
   def generate(repository_name)
     repository_list = Canpe::RepositoryStore.repository_list
     unless repository_list.include? repository_name
@@ -18,6 +19,7 @@ class Canpe::Command < Thor
   end
 
   desc "destroy repo_name", "Delete generated files"
+
   def destroy(repository_name)
     repository_list = Canpe::RepositoryStore.repository_list
     unless repository_list.include? repository_name
@@ -29,10 +31,24 @@ class Canpe::Command < Thor
   end
 
   desc "list", "List up registered repositorys"
+
   def list
     repository_list = Canpe::RepositoryStore.repository_list
     repository_list.each do |repository_name|
       puts repository_name
     end
+  end
+
+  desc "open", "open registered repository"
+
+  def open(repository_name)
+    repository_list = Canpe::RepositoryStore.repository_list
+    unless repository_list.include? repository_name
+      $stderr.puts "Could not find repository \"#{repository_name}\"."
+      exit 1
+    end
+
+    repo = Canpe::Repository.new(repository_name)
+    `open #{repo.repository_url}`
   end
 end
